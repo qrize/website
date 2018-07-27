26b13a96b648388df8758884c1bdc6f72df3d6af
Sergey Goliney <sergey@goliney.com>
2018-07-27 10:55:29 +0300

    Add :tada:

--- src/1__tryit/tryit-container.js
+++ src/1__tryit/tryit-container.js
@@ -7,7 +7,7 @@ const TryItContainer = () => (
     <section className="section">
       <header>
         <h1 className="title">
-          <strong>QR code</strong> and <strong>URL shortener</strong> got married.
+          <strong>QR code</strong> and <strong>URL shortener</strong> got married 🎉
         </h1>
         <p class="try-qrize">
           Create <strong>tiny QR codes</strong> for your web-pages. Check it out:
